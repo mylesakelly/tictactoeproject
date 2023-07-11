73991e87f8dc1ba3b7f4a147b0ff516308b1318f
Myles Kelly <mylesastoria2@gmail.com>
2023-07-11 19:33:02 -0400

    added comments to code

--- tictactoegame/tttgame/src/App.js
+++ tictactoegame/tttgame/src/App.js
@@ -1,31 +1,52 @@
+// imports the useState hook from the react library that will allow us to change the state of the component on the DOM
 import { useState } from 'react';
 
+// functional component named square that takes in two props: value and onSquareClick
 function Square({value, onSquareClick}) {
   return (
+    // When the button is clicked, it will invoke the onSquareClick function passed as a prop.
     <button className="square" onClick={onSquareClick}>
+      {/* the value prop will be the value of the square, either X or O */}
       {value}
     </button>
   );
 }
 
+// defining the Board component as the main functional component
+// this function will represent the gameboard
 export default function Board() {
+  // xIsNext is the initialized variable using the useState hook. 
+  // the function setXIsNext is used to update the value of the state
   const [xIsNext, setXIsNext] = useState(true);
+  // initializes a state variable called squares and the initial value of squares is set to an array with null values
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  // i is being used so that the square can be updated with "x" or "o" accordingly.
   function handleClick(i) {
+
+    // This line checks if there is a winner or if the square at index i already has a value. 
+    // If either condition is true, the function returns early and does nothing.
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+    // This line creates a copy of the squares array using the slice() method. 
+    // This is done to avoid directly modifying the original array.
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
+    // This line updates the squares state with the modified nextSquares array.
     setSquares(nextSquares);
+
+    // This line toggles the value of xIsNext by negating its current value using the logical NOT operator (!).
+     // It ensures that the next turn will be played by the opposite player.
     setXIsNext(!xIsNext);
   }
-
+// This line invokes the calculateWinner function, passing the squares array as an argument, 
+// and assigns the result to the winner variable. 
+// The calculateWinner function determines if there is a winner in the game.
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -56,6 +77,7 @@ export default function Board() {
   );
 }
 
+// checks if there is a winner by checking the value of the squares
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -67,6 +89,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
